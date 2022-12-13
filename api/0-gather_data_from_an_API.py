10e160daaa5649522fb1faebe680dd0ebf1801c1
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-12-12 20:37:33 -0500

    update-api

---
+++ api/0-gather_data_from_an_API.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""  using this REST API, for a given employee ID, returns information about his/her TODO """
+import requests
+
+def gathe_data_api():
+    pass
+
+if __name__ == "__main__":
+    gathe_data_api()
